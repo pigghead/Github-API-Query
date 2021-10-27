6815b662c9c664039f325498393b00b2f56f61c5
Justin Armstrong <jxa1762@rit.edu>
2021-10-27 16:16:36 -0400

    Updates

---
+++ precache-manifest.24b8766a756e80611049ff020a402171.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eb4d89b9056ac57414214e72e6f932",
+    "url": "/Github-API-Query/index.html"
+  },
+  {
+    "revision": "9dc06f2a0582bd6e51bf",
+    "url": "/Github-API-Query/static/css/main.bece7d99.chunk.css"
+  },
+  {
+    "revision": "224c301db54ebd5c7aff",
+    "url": "/Github-API-Query/static/js/2.63418e45.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Github-API-Query/static/js/2.63418e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6538a54b45ce89c5b3a",
+    "url": "/Github-API-Query/static/js/3.47eaad5d.chunk.js"
+  },
+  {
+    "revision": "9dc06f2a0582bd6e51bf",
+    "url": "/Github-API-Query/static/js/main.35303e42.chunk.js"
+  },
+  {
+    "revision": "533f924bfba3d50b5ae3",
+    "url": "/Github-API-Query/static/js/runtime-main.9148158f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Github-API-Query/static/media/logo.06e73328.svg"
+  }
+]);
